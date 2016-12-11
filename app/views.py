b3277ebc00cdba2288c93c612a99789a2c624a7a
victoriaweaver <vweaver2013@gmail.com>
2016-12-11 15:55:09 -0500

    Add static page talpacket

--- app/views.py
+++ app/views.py
@@ -2,20 +2,7 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Victoria'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+
+@app.route('/talpacket')
+def talpacket():
+    return render_template('talpacket.html')  # render a template
