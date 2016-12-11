6dd99ac16378c4c248b4c839b85df82a9b969b03
victoriaweaver <vweaver2013@gmail.com>
2016-12-11 18:46:43 -0500

    Change footer text and separate home page and tal page

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+def home():
+    return render_template('base.html')  # render a template
 
 @app.route('/talpacket')
 def talpacket():
