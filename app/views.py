2fcce0ca99f1243aed8de72f7cd408650a09701b
victoriaweaver <vweaver2013@gmail.com>
2016-12-12 20:56:33 -0500

    Add image and change navbar title on tal page

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ from app import app
 
 @app.route('/')
 def home():
-    return render_template('base.html')  # render a template
+    return render_template('base.html', title='Is My Door Open?')  # render a template
 
 @app.route('/talpacket')
 def talpacket():
-    return render_template('talpacket.html')  # render a template
+    return render_template('talpacket.html',  title='Is Tal\'s Door Open?')  # render a template
