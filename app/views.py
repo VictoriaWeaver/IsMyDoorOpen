885d191aedaf32b1836a17682351a14f504e9cb2
victoriaweaver <vweaver2013@gmail.com>
2016-12-12 21:54:40 -0500

    Add schedule page

--- app/views.py
+++ app/views.py
@@ -3,8 +3,12 @@ from app import app
 
 @app.route('/')
 def home():
-    return render_template('base.html', title='Is My Door Open?')  # render a template
+    return render_template('base.html')  # render a template
 
 @app.route('/talpacket')
 def talpacket():
     return render_template('talpacket.html',  title='Is Tal\'s Door Open?')  # render a template
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')  # render a template
